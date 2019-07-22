a6f82e2e1a6f73d3f5626f5c7dd1c8df77bd7096
marduzca <miguel.arduz@thoughtworks.com>
2019-07-22 16:03:02 +0200

    Make times method return Dollar object to eliminate side effects

--- src/test/MoneyMainKtTest.kt
+++ src/test/MoneyMainKtTest.kt
@@ -8,9 +8,12 @@ class MoneyMainKtTest {
 
     @Test
     fun testMultiplication() {
-        var five = Dollar(5)
-        five.times(2)
-        assertEquals(10, five.amount)
+        val five = Dollar(5)
+        var product = five.times(2)
+        assertEquals(10, product.amount)
+
+        product = five.times(3)
+        assertEquals(15, product.amount)
     }
 
 }
