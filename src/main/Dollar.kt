7c192b65e79c9231ce8f13324c54245b1549928b
marduzca <miguel.arduz@thoughtworks.com>
2019-07-22 16:22:59 +0200

    Add equality test and make it green

--- src/main/Dollar.kt
+++ src/main/Dollar.kt
@@ -4,4 +4,8 @@ class Dollar(var amount: Int) {
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
     }
+
+    fun equals(dollar: Dollar): Boolean {
+        return this.amount == dollar.amount
+    }
 }
--- src/test/MoneyMainKtTest.kt
+++ src/test/MoneyMainKtTest.kt
@@ -3,6 +3,8 @@ package test
 import main.Dollar
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class MoneyMainKtTest {
 
@@ -16,4 +18,9 @@ class MoneyMainKtTest {
         assertEquals(15, product.amount)
     }
 
+    @Test
+    fun testEquality() {
+        assertTrue(Dollar(5).equals(Dollar(5)))
+        assertFalse(Dollar(5).equals(Dollar(6)))
+    }
 }
