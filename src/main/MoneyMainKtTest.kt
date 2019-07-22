297b26caf9a0069427b2c07a628bbfa54f893f42
marduzca <miguel.arduz@thoughtworks.com>
2019-07-22 14:11:57 +0200

    Initialize project

---
+++ src/main/MoneyMainKtTest.kt
@@ -0,0 +1,15 @@
+package main
+
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+object MoneyMainKtTest {
+
+    fun testMultiplication() {
+//        Dollar five = new Dollar(5)
+//        five.times(2)
+//        assertEquals(10, five..amount)
+        assertTrue { true }
+    }
+
+}
